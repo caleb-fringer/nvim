ae5fdef9c47c528d0beb90617e6286c902e6923c
CalebF98 <cfringer98@gmail.com>
2023-11-16 21:00:44 -0800

    Added window command keybinds.

--- lua/sepulchre/keybindings.lua
+++ lua/sepulchre/keybindings.lua
@@ -1,3 +1,23 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set({"n", "i"}, "<C-s>", vim.cmd.write)
+
+keybindings = {
+    { modes="n", lhs="<leader>pv", cmd=vim.cmd.Ex },
+    { modes={"n","i"}, lhs="<leader>pv", cmd=vim.cmd.Ex },
+}
+
+function wincmd_keybind_factory(arg)
+    return {
+	modes="n",
+	lhs="<leader>w" .. arg,
+	cmd=function() vim.cmd.wincmd(arg) end
+    }
+end
+
+for _, i in pairs({"h","j","k","l"}) do
+    table.insert(keybindings, wincmd_keybind_factory(i))
+end
+
+for _, bind in pairs(keybindings) do
+    vim.keymap.set(bind.modes, bind.lhs, bind.cmd)
+end
+
