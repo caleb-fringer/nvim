e111ffc7f83f9299f53b2b2bdfeba37f40290fe5
CalebF98 <cfringer98@gmail.com>
2023-11-16 16:41:08 -0800

    Added hybrid relative numbers

--- lua/sepulchre/init.lua
+++ lua/sepulchre/init.lua
@@ -4,5 +4,8 @@ require("sepulchre.lazy")
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+-- Enable both for hybrid line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.cmd("colorscheme rose-pine")
