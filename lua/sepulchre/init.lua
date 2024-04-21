896bc8d9b8cef8ada2a19127fd98079f57d9179c
CalebF98 <cfringer98@gmail.com>
2024-04-20 19:37:09 -0700

    Set softtabstop = 0 to prevent neovim from mixing tabs and spaces.

--- lua/sepulchre/init.lua
+++ lua/sepulchre/init.lua
@@ -2,7 +2,7 @@ require("sepulchre.keybindings")
 require("sepulchre.lazy")
 
 vim.opt.shiftwidth = 4
-vim.opt.softtabstop = -1
+vim.opt.softtabstop = 0
 vim.opt.expandtab = true
 -- Enable both for hybrid line numbers
 vim.opt.number = true
