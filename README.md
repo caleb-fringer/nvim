ffc9e695da28a45940b71fb32166a19c3bc67ea7
Caleb Fringer <cfringer98@gmail.com>
2024-05-08 15:29:19 -0700

    Added telescope-file-browser, configured nvim-web-devicons to use NerdFonts for file icons.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 - Add proper tab management
 - Fix Java debugger
 - Implement all wincmd keybindings
-- Add proper filetree plugin
+~~- Add proper filetree plugin~~
 - Add terminal/task running plugin
-
+~~- Brace/bracket completion~~
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,17 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local extensions = telescope.extensions
+
+telescope.setup{
+    extensions = {
+        file_browser = {
+            hijack_netrw = true
+        }
+    }
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fe', extensions.file_browser.file_browser, {})
