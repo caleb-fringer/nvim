056eadf8e89249f96579a4d3e69f27692bd38209
CalebF98 <cfringer98@gmail.com>
2024-08-05 17:47:51 -0700

    Added descriptions, find keymap keybindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,8 +10,9 @@ telescope.setup{
     }
 }
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fe', extensions.file_browser.file_browser, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find help tags"})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = "Find keymap" })
+vim.keymap.set('n', '<leader>fe', extensions.file_browser.file_browser, { desc = "Browse directory" })
