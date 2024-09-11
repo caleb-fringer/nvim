747ded4f752c8949db0a847c7f329ec8e49514a6
Caleb Fringer <cfringer98@gmail.com>
2024-09-11 16:55:11 -0700

    Added ignore pairs for single quote and backticks for Lisp

--- after/plugin/nvim-autopairs.lua
+++ after/plugin/nvim-autopairs.lua
@@ -1,6 +1,12 @@
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
+
 cmp.event:on(
     'confirm done',
     cmp_autopairs.on_confirm_done()
 )
+
+local npairs = require('nvim-autopairs')
+-- Add rules for Lisp and Scheme programs
+npairs.get_rules("'")[1].not_filetypes = { "scheme", "lisp" }
+npairs.get_rules("`")[1].not_filetypes = { "scheme", "lisp" }
