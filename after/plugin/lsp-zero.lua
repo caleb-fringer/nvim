c1e18ce2370f8bd6f395290e3abfd55f15f22213
CalebF98 <cfringer98@gmail.com>
2024-10-03 23:15:13 -0700

    Fix TypeScript integration with Volar

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -11,5 +11,33 @@ require('mason-lspconfig').setup({
   ensure_installed = { "clangd", "ts_ls", "lua_ls", "pylsp", "jdtls", "volar" },
   handlers = {
     lsp_zero.default_setup,
+    ts_ls = function()
+        local vue_typescript_plugin = require('mason-registry')
+            .get_package('vue-language-server')
+            :get_install_path()
+            .. '/node_modules/@vue/language-server'
+            .. '/node_modules/@vue/typescript-plugin'
+
+        require('lspconfig').ts_ls.setup({
+            init_options = {
+              plugins = {
+                {
+                  name = "@vue/typescript-plugin",
+                  location = vue_typescript_plugin,
+                  languages = {'javascript', 'typescript', 'vue'}
+                },
+              }
+            },
+            filetypes = {
+              'javascript',
+              'javascriptreact',
+              'javascript.jsx',
+              'typescript',
+              'typescriptreact',
+              'typescript.tsx',
+              'vue',
+            },
+          })
+        end
   },
 })
