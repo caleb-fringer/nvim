e3ed9a55319dac0b7b48eca53b584660ac110f7d
Caleb Fringer <cfringer98@gmail.com>
2025-02-12 21:16:08 -0800

    Add basic harpoon setup

---
+++ after/plugin/harpoon.lua
@@ -0,0 +1,18 @@
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+vim.keymap.set("n", "<Tab>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<S-Tab>", function() harpoon:list():prev() end)
+
+-- basic telescope config
