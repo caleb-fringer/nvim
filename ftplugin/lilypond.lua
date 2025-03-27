1c4727a40ee4d6f7907a8ae1040d74081e1fc7b8
Caleb Fringer <cfringer98@gmail.com>
2025-03-27 14:34:25 -0700

    Print outputfile name on success.

--- ftplugin/lilypond.lua
+++ ftplugin/lilypond.lua
@@ -3,10 +3,13 @@ local id = vim.api.nvim_create_augroup("LilypondFile", {})
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     group = id,
     callback = function(e)
-        print("Engraving score...l")
-        local result = vim.system({ "lilypond", e.file }):wait()
+        print("Engraving score...")
+        local outfile = e.file:gsub(".ly", ".pdf")
+        local result = vim.system({ "lilypond", e.file, "-o", outfile }):wait()
         if result.code ~= 0 then
             print("Error engraving score: " .. result.stderr)
+        else
+            print("Score saved to " .. outfile)
         end
     end
 })
