e3652dfed9a7cdb01dccb3800db06696895b86ce
Caleb Fringer <cfringer98@gmail.com>
2025-03-27 19:22:40 -0700

    Fix outfile name printing

--- ftplugin/lilypond.lua
+++ ftplugin/lilypond.lua
@@ -4,11 +4,11 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     group = id,
     callback = function(e)
         print("Engraving score...")
-        local outfile = e.file:gsub(".ly", ".pdf")
-        local result = vim.system({ "lilypond", "-o", outfile, e.file }):wait()
+        local result = vim.system({ "lilypond", e.file }):wait()
         if result.code ~= 0 then
             print("Error engraving score. Please check :messages")
         else
+            local outfile = e.file:gsub(".ly", ".pdf")
             print("Score saved to " .. outfile)
         end
     end
