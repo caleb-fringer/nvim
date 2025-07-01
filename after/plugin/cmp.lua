b30484c6728031000ff2bbf5fd64af6175df81ac
Caleb Fringer <cfringer98@gmail.com>
2025-07-01 16:52:03 -0700

    Setup LuaSnip + SuperTab completion
    Setup LuaSnip + SuperTab completion
    
    Fix fucked up

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,25 +1,51 @@
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 
 cmp.setup({
-    sources = {
-        {name = 'nvim_lsp'},
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
     },
-    mapping = cmp.mapping.preset.insert({
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            local col = vim.fn.col('.') - 1
-
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item({behavior = 'select'})
-            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                fallback()
+                if luasnip.expandable() then
+                    luasnip.expand()
+                else
+                    cmp.confirm({
+                        select = true,
+                    })
+                end
             else
-                cmp.complete()
+                fallback()
             end
-        end, {'i', 's'}),
+        end),
 
-        ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    })
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+            else
+                print('fuck')
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
 })
 
 cmp.setup.filetype({ 'lisp' }, {
