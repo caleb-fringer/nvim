ed8e23a779dda03a2b6441f3110415b1f56fd0c4
Caleb Fringer <cfringer98@gmail.com>
2026-05-21 19:53:19 -0700

    Add script during init to fetch latest config.

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
+local configrepo = "https://github.com/caleb-fringer/nvim"
+local cmd = { "git", "pull", configrepo }
+local ok, sys_obj = pcall(vim.system, cmd)
+if not ok then
+    local msg = table.concat(cmd, " ")
+    vim.notify("Error executing command: " .. msg)
+end
+local ok, result = pcall(sys_obj.wait, sys_obj, 5000)
+if not ok or result.code ~= 0 then
+    vim.notify("Error fetching latest config from Github: " .. result.stderr)
+end
+
 require("sepulchre")
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
