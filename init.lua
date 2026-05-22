b6512c3f64fab74146dc66349dfa45c953db2304
Caleb Fringer <cfringer98@gmail.com>
2026-05-21 20:43:20 -0700

    Add log level to bootstrap error msgs

--- init.lua
+++ init.lua
@@ -2,11 +2,19 @@ local cmd = { "git", "-C", vim.fn.stdpath("config"), "pull" }
 local ok, sys_obj = pcall(vim.system, cmd)
 if not ok then
     local msg = table.concat(cmd, " ")
-    vim.notify("Error executing command: " .. msg)
+    vim.notify(
+        "Error executing git pull cmd: " .. msg,
+        vim.log.levels.ERROR
+    )
 end
-local ok, result = pcall(sys_obj.wait, sys_obj, 5000)
+
+local result
+ok, result = pcall(sys_obj.wait, sys_obj, 5000)
 if not ok or result.code ~= 0 then
-    vim.notify("Error fetching latest config from Github: " .. result.stderr)
+    vim.notify(
+        "Couldn't fetch latest config from Github: " .. result.stderr,
+        vim.log.levels.WARN
+    )
 end
 
 require("sepulchre")
