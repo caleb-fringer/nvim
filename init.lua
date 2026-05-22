b19148938e266f2909cc4a467d68579d7c19dd04
Caleb Fringer <cfringer98@gmail.com>
2026-05-21 22:36:35 -0700

    Refactor synchronzing repository to be asynchronous, preserving startup times.

--- init.lua
+++ init.lua
@@ -1,19 +1,21 @@
 local cmd = { "git", "-C", vim.fn.stdpath("config"), "pull" }
-local ok, sys_obj = pcall(vim.system, cmd)
-if not ok then
+
+local callback = function(out)
+    if out.code ~= 0 then
+        vim.schedule(function()
+            vim.notify(
+                "Couldn't fetch latest config from Github: " .. out.stderr,
+                vim.log.levels.WARN
+            )
+        end)
+    end
+end
+
+if not pcall(vim.system, cmd, callback) then
     local msg = table.concat(cmd, " ")
     vim.notify(
         "Error executing git pull cmd: " .. msg,
         vim.log.levels.ERROR
-    )
-end
-
-local result
-ok, result = pcall(sys_obj.wait, sys_obj, 5000)
-if not ok or result.code ~= 0 then
-    vim.notify(
-        "Couldn't fetch latest config from Github: " .. result.stderr,
-        vim.log.levels.WARN
     )
 end
 
