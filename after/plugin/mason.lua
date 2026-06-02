cc1cc1a6a0df5999e50d71fb796ff827500cb032
Caleb Fringer <cfringer98@gmail.com>
2026-06-02 12:12:12 -0700

    Add Astro LSP and TreeSitter setup.

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,6 +2,7 @@ require('mason').setup()
 require('mason-lspconfig').setup({
     ensure_installed = {
         "ansiblels",
+        "astro",
         "clangd",
         "clojure_lsp",
         "elixirls",
--- after/plugin/nvim-treesitter.lua
+++ after/plugin/nvim-treesitter.lua
@@ -1,4 +1,5 @@
 local parsers = {
+    'astro',
     'awk',
     'bash',
     'bibtex',
