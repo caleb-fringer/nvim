fb023b7c8d95808d29fa99933ad6ed8d5440e650
Caleb Fringer <cfringer98@gmail.com>
2026-08-27 11:01:46 -0700

    Conditionally resolve subvolume symlinks for dap-go, as dlv requires absolute paths for breakpoints.

--- after/plugin/dap-go.lua
+++ after/plugin/dap-go.lua
@@ -1,1 +1,20 @@
+local dap = require("dap")
+
 require("dap-go").setup()
+
+for _, config in ipairs(dap.configurations.go or {}) do
+    -- Conditionally resolve subvolume symlinks to make sure breakpoints
+    -- reference the absolute path, as required by dlv.
+    if vim.fn.resolve("/home/caleb/src") == "/data/src" then
+        config.substitutePath = {
+            {
+                from = "/home/caleb/src",
+                to = "/data/src",
+            },
+            {
+                from = "/data/src",
+                to = "/home/caleb/src",
+            },
+        }
+    end
+end
